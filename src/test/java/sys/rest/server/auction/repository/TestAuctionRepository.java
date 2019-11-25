3904c6b68acd3e57fc69985dab4768a410fc5e2a
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-11-25 03:18:37 +0000

    Update tests, handle having previous databases so that local tests do not fail

--- src/test/java/sys/rest/server/auction/repository/TestAuctionRepository.java
+++ src/test/java/sys/rest/server/auction/repository/TestAuctionRepository.java
@@ -3,8 +3,13 @@ package test.java.sys.rest.server.auction.repository;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.file.Files;
+
 import javax.ws.rs.core.Response.Status;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,7 +30,13 @@ public class TestAuctionRepository {
 	private Gson gson;
 	
 	@BeforeAll
-	public static void initAuctionRepositoryServer() {
+	public static void initAuctionRepositoryServer() throws InterruptedException {
+    	File file = new File("res/database");
+    	File[] files = file.listFiles();
+    	for (File tempFile : files) {
+			tempFile.renameTo(new File(tempFile.getAbsolutePath() + ".bak"));
+		}
+    	
     	AuctionRepositoryServer.main(null);
 	}
 	
@@ -36,15 +47,46 @@ public class TestAuctionRepository {
         RestAssured.port = 8080;
     }
  
+    @AfterAll
+    public static void cleanup() {
+    	File file = new File("res/database");
+    	File[] files = file.listFiles( new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				// TODO Auto-generated method stub
+				return name.endsWith(".db");
+			}
+		});
+    	for (File tempFile : files) {
+			tempFile.delete();
+		}
+    	
+    	file = new File("res/database");
+    	files = file.listFiles( new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				// TODO Auto-generated method stub
+				System.err.println("Found: " + name);
+				return name.endsWith(".bak");
+			}
+		});
+    	for (File tempFile : files) {
+			tempFile.renameTo(
+					new File(tempFile.getAbsolutePath().substring(0, tempFile.getAbsolutePath().length() - 4)));
+		}
+    }
+    
     @Test
-    public void testConnection() {
+    public void testConnection() throws InterruptedException {
         get("/products-auctions/all")
         .then()
         .statusCode(Status.NO_CONTENT.getStatusCode());
     }
     
     @Test
-    public void testSimpleAuctionAdd() {
+    public void testSimpleAuctionAdd() throws InterruptedException {
     	given()
     	.contentType(ContentType.JSON)
     	.body(AuctionRepositoryTestData.auction0002)
@@ -55,7 +97,7 @@ public class TestAuctionRepository {
     }
     
     @Test
-    public void testGetAuctionsFromPreviousAdd() {
+    public void testGetAuctionsFromPreviousAdd() throws InterruptedException {
         get("/products-auctions/all")
         .then()
         .statusCode(Status.OK.getStatusCode())
@@ -63,12 +105,26 @@ public class TestAuctionRepository {
     }
     
     @Test
-    public void testGetAuctionAddedFromPreviousAdd() {
+    public void testGetAuctionAddedFromPreviousAdd() throws InterruptedException {
+    	Auction testAuction = buildAuction(AuctionRepositoryTestData.auction0002);
         get("/products-auctions/all/auction0002")
         .then()
         .statusCode(Status.OK.getStatusCode())
         .contentType(ContentType.JSON)
-        .body("auctionID", equalTo("auction0002"));
+        .body("auctionID", equalTo(testAuction.getAuctionID()))
+        .body("auctionSerialNumber", equalTo(testAuction.getAuctionSerial()))
+        .body("auctionDescription", equalTo(testAuction.getAuctionDescription()))
+//        .body("auctionBidType", equalTo(testAuction.getAuctionBidType()))
+//        .body("currentBidValue", equalTo(testAuction.getCurrentBidValue()))
+//        .body("minAmountBidValue", equalTo(testAuction.getMinAmountBidValue()))
+//        .body("maxAmountBidValue", equalTo(testAuction.getMaxAmountBidValue()))
+        .body("numMaxAuctionBidsAllowed", equalTo(testAuction.getNumMaxAuctionBidsAllowed()))
+        .body("auctionTimestampStart", equalTo(testAuction.getAuctionTimestampStart()))
+        .body("auctionTimestampLimit", equalTo(testAuction.getAuctionTimestampLimit()))
+        .body("auctionIsOpen", equalTo(testAuction.verifyIfAuctionIsOpen()))
+        .body("productID", equalTo(testAuction.getProductID()))
+        .body("productName", equalTo(testAuction.getProductName()))
+        .body("productOwnerUserClientID", equalTo(testAuction.getProductOwnerUserClientID()));
     }
     
     private Auction buildAuction(String json) {
