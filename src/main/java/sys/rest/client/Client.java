949a6376a1792273e778a2c8d1122099145276fe
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-27 19:48:10 +0000

    Some features added

--- src/main/java/sys/rest/client/Client.java
+++ src/main/java/sys/rest/client/Client.java
@@ -1,14 +1,6 @@
 package main.java.sys.rest.client;
 
-import java.net.URI;
-
-import javax.ws.rs.core.UriBuilder;
-
-import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-
 import main.java.api.rest.client.ClientAPI;
-import main.java.sys.rest.server.auction.repository.AuctionRepositoryServer;
 
 public class Client implements ClientAPI {
 
