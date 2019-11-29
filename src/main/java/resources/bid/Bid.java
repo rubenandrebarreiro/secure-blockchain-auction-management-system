19dcaeddf9b45b013808a3edad661461b933ba6d
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-29 09:32:24 +0000

    Some minor improvements in Bid added

--- src/main/java/resources/bid/Bid.java
+++ src/main/java/resources/bid/Bid.java
@@ -17,14 +17,15 @@ public class Bid {
 	@DatabaseField
 	private long bidTimestamp;
 	
-	private byte[] serializedBytes;
+	private byte[] bidSerializedBytes;
 
 	
 	public Bid() {
-		// TODO Auto-generated constructor stub
+		
+		// Obsolete/Empty Consructor
+		
 	}
 	
-
 	public Bid(long bidID, String bidderUserClientID, double bidValue) {
 
 		this.bidID = bidID;
@@ -33,7 +34,14 @@ public class Bid {
 		this.bidTimestamp = System.currentTimeMillis();
 
 	}
-
+	
+	public Bid(byte[] bidSerializedBytes) {
+		
+		this.bidSerializedBytes = bidSerializedBytes;
+		
+	}
+	
+	
 	public long getBidID() {
 		return this.bidID;
 	}
@@ -66,20 +74,20 @@ public class Bid {
 		this.bidTimestamp = bidTimestamp;
 	}
 	
-	public byte[] getSerializedBytes() {
-		return this.serializedBytes;
+	public byte[] getBidSerializedBytes() {
+		return this.bidSerializedBytes;
 	}
 
-	public void setSerializedBytes(byte[] serializedBytes) {
-		this.serializedBytes = serializedBytes;
+	public void setBidSerializedBytes(byte[] bidSerializedBytes) {
+		this.bidSerializedBytes = bidSerializedBytes;
 	}
 	
 	public void doSerialization() {
 		
-		int sizeOfSerializedBytes = ( CommonUtils.LONG_IN_BYTES_LENGTH + this.bidderUserClientID.length()
+		int sizeOfBidSerializedBytes = ( CommonUtils.LONG_IN_BYTES_LENGTH + this.bidderUserClientID.length()
 		                            + CommonUtils.DOUBLE_IN_BYTES_LENGTH + CommonUtils.LONG_IN_BYTES_LENGTH );
 		
-		this.serializedBytes = new byte[sizeOfSerializedBytes];
+		this.bidSerializedBytes = new byte[sizeOfBidSerializedBytes];
 		
 		byte[] bidIDSerialized = CommonUtils.fromLongToByteArray(this.bidID);
 		byte[] bidderUserClientIDSerialized = CommonUtils.fromStringToByteArray(this.bidderUserClientID);
@@ -100,29 +108,74 @@ public class Bid {
 		
 		// Fills the byte array of the Bid serialized with its ID,
 		// From the initial position to the corresponding to the length of Bid's ID
-		System.arraycopy(bidIDSerialized, 0, this.serializedBytes, serializationOffset, bidIDSerialized.length);
+		System.arraycopy(bidIDSerialized, 0, this.bidSerializedBytes, serializationOffset, bidIDSerialized.length);
 		serializationOffset += bidIDSerialized.length;
 		
 		// Fills the byte array of the Bid serialized with its User/Client Bidder's ID,
 		// From the position corresponding to the length of Bid's ID to
 		// the corresponding of the length of Bid's User/Client Bidder's ID
-		System.arraycopy(bidderUserClientIDSerialized, 0, this.serializedBytes, serializationOffset, bidderUserClientIDSerialized.length);
+		System.arraycopy(bidderUserClientIDSerialized, 0, this.bidSerializedBytes, serializationOffset, bidderUserClientIDSerialized.length);
 		serializationOffset += bidderUserClientIDSerialized.length;
 
 		// Fills the byte array of the Bid serialized with its Value Amount,
 		// From the position corresponding to the length of Bid's User/Client Bidder's ID to
 		// the corresponding of the length of Bid's Value Amount
-		System.arraycopy(bidValueSerialized, 0, this.serializedBytes, serializationOffset, bidValueSerialized.length);
+		System.arraycopy(bidValueSerialized, 0, this.bidSerializedBytes, serializationOffset, bidValueSerialized.length);
 		serializationOffset += bidValueSerialized.length;
 
-		// Fills the byte array of the Bid serialized with its Timestampo,
+		// Fills the byte array of the Bid serialized with its Timestamp,
 		// From the position corresponding to the length of Bid's Value Amount to
 		// the corresponding of the length of Bid's Timestamp
-		System.arraycopy(bidTimestampSerialized, 0, this.serializedBytes, serializationOffset, bidTimestampSerialized.length);
+		System.arraycopy(bidTimestampSerialized, 0, this.bidSerializedBytes, serializationOffset, bidTimestampSerialized.length);
 		
 	}
 
-	public void undoSerialization() {
+	public void undoSerialization(int sizeOfBidderUserClientID) {
+		
+		byte[] bidIDSerialized = new byte[CommonUtils.LONG_IN_BYTES_LENGTH];
+		byte[] bidderUserClientIDSerialized = new byte[sizeOfBidderUserClientID];
+		byte[] bidValueSerialized = new byte[CommonUtils.DOUBLE_IN_BYTES_LENGTH];
+		byte[] bidTimestampSerialized = new byte[CommonUtils.LONG_IN_BYTES_LENGTH];
+		
+		
+		// Operations to Fill a Byte Array, with the following parameters:
+		// 1) src - The source of the array to be copied
+		// 2) srcPos - The position from the array to be copied, representing the first element to be copied
+		// 3) dest - The destination of the array to be copied
+		// 4) destPos - The position of the array where will be placed the new copy,
+		//              representing the first element where new data will be placed
+		// 5) length - The length of the data to be copied from the source array to the destination array
+		
+		// The offset related to fulfilment of the serialization process
+		int serializationOffset = 0;
+		
+		// Fills the byte array of the Bid's ID serialized from the Bid serialized,
+		// From the initial position to the corresponding to the length of Bid's serialized ID
+		System.arraycopy(this.bidSerializedBytes, serializationOffset, bidIDSerialized, 0, bidIDSerialized.length);
+		serializationOffset += bidIDSerialized.length;
+		
+		// Fills the byte array of the Bid's User/Client Bidder's ID serialized from the Bid serialized,
+		// From the position corresponding to the length of Bid's ID serialized to
+		// the corresponding of the length of Bid's User/Client Bidder's ID serialized
+		System.arraycopy(this.bidSerializedBytes, serializationOffset, bidderUserClientIDSerialized, 0, bidderUserClientIDSerialized.length);
+		serializationOffset += bidderUserClientIDSerialized.length;
+
+		// Fills the byte array of the Bid's Value Amount from the Bid serialized,
+		// From the position corresponding to the length of Bid's User/Client Bidder's ID serialized to
+		// the corresponding of the length of Bid's Value Amount serialized
+		System.arraycopy(this.bidSerializedBytes, serializationOffset, bidValueSerialized, 0, bidValueSerialized.length);
+		serializationOffset += bidValueSerialized.length;
+
+		// Fills the byte array of the Bid's Timestamp from the Bid serialized,
+		// From the position corresponding to the length of Bid's Value Amount serialized to
+		// the corresponding of the length of Bid's Timestamp serialized
+		System.arraycopy(this.bidSerializedBytes, serializationOffset, bidTimestampSerialized, 0, bidTimestampSerialized.length);
+		
+		
+		this.bidID = CommonUtils.fromByteArrayToLong(bidIDSerialized);
+		this.bidderUserClientID = CommonUtils.fromByteArrayToString(bidderUserClientIDSerialized);
+		this.bidValue = CommonUtils.fromByteArrayToDouble(bidValueSerialized);
+		this.bidTimestamp = CommonUtils.fromByteArrayToLong(bidTimestampSerialized);
 		
 	}
 }
