29adc0b5e0f8e431922ef6c44a0c6bcb775b34a5
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-29 23:52:16 +0000

    Some minor improvements added

--- src/main/java/messages/secure/challenge/ChallengeMessage.java
+++ src/main/java/messages/secure/challenge/ChallengeMessage.java
@@ -2,18 +2,38 @@ package main.java.messages.secure.challenge;
 
 import main.java.messages.secure.challenge.block.ChallengeMessageBlock;
 import main.java.messages.secure.challenge.metaheader.ChallengeMessageMetaHeader;
+import main.java.messages.secure.common.header.SecureCommonHeader;
 
 public class ChallengeMessage {
 	
 	private ChallengeMessageMetaHeader challengeMessageMetaHeader;
 	
+	private SecureCommonHeader secureCommonHeader;
+	
 	private ChallengeMessageBlock challengeMessageBlock;
 	
+	private byte[] challengeMessageSerialized;
+			
 	
-	public ChallengeMessage(ChallengeMessageMetaHeader challengeMessageMetaHeader, ChallengeMessageBlock challengeMessageBlock) {
+	public ChallengeMessage(ChallengeMessageMetaHeader challengeMessageMetaHeader,
+			                SecureCommonHeader secureCommonHeader,
+			                ChallengeMessageBlock challengeMessageBlock) {
 		
 		this.challengeMessageMetaHeader = challengeMessageMetaHeader;
+		this.secureCommonHeader = secureCommonHeader;
 		this.challengeMessageBlock = challengeMessageBlock;
+		
+		this.challengeMessageSerialized = null;
+		
+	}
+	
+	public ChallengeMessage(byte[] challengeMessageSerialized) {
+		
+		this.challengeMessageSerialized = challengeMessageSerialized;
+
+		this.challengeMessageMetaHeader = null;
+		this.secureCommonHeader = null;
+		this.challengeMessageBlock = null;
 		
 	}
 	
@@ -25,6 +45,14 @@ public class ChallengeMessage {
 		this.challengeMessageMetaHeader = challengeMessageMetaHeader;
 	}
 	
+	public SecureCommonHeader getSecureCommonHeader() {
+		return this.secureCommonHeader;
+	}
+	
+	public void setSecureCommonHeader(SecureCommonHeader secureCommonHeader) {
+		this.secureCommonHeader = secureCommonHeader;
+	}
+	
 	public ChallengeMessageBlock getChallengeMessageBlock() {
 		return this.challengeMessageBlock;
 	}
@@ -33,4 +61,19 @@ public class ChallengeMessage {
 		this.challengeMessageBlock = challengeMessageBlock;
 	}
 	
+	public byte[] getChallengeMessageSerialized() {
+		return this.challengeMessageSerialized;
+	}
+	
+	public void setChallengeMessageSerialized(byte[] challengeMessageSerialized) {
+		this.challengeMessageSerialized = challengeMessageSerialized;
+	}
+	
+	public void doChallengeMessageSerialization() {
+		// TODO
+	}
+	
+	public void undoChallengeMessageSerialization() {
+		// TODO
+	}
 }
--- src/main/java/messages/secure/common/header/SecureCommonHeader.java
+++ src/main/java/messages/secure/common/header/SecureCommonHeader.java
@@ -1,26 +1,43 @@
 package main.java.messages.secure.common.header;
 
+import main.java.common.utils.CommonUtils;
+
 public abstract class SecureCommonHeader {
 	
 	private byte versionID;
 	
 	private byte messageType;
 	
+	private long timestamp;
+	
 	private byte[] secureCommonHeaderSerialized;
 	
-	public SecureCommonHeader(byte versionID, byte messageType) {
+	public SecureCommonHeader(byte versionID, byte messageType, long timestamp) {
 		
 		this.versionID = versionID;
 		this.messageType = messageType;
-		this.secureCommonHeaderSerialized = new byte[] { versionID, messageType };
+		this.timestamp = timestamp;
+		
+		byte[] timestampSerialized = CommonUtils.fromLongToByteArray(timestamp);
+		this.secureCommonHeaderSerialized = new byte[] { versionID, messageType,
+	 													 timestampSerialized[0], timestampSerialized[1],
+														 timestampSerialized[2], timestampSerialized[3],
+														 timestampSerialized[4], timestampSerialized[5],
+														 timestampSerialized[6], timestampSerialized[7] };
 		
 	}
 	
 	public SecureCommonHeader(byte[] secureCommonHeaderSerialized) {
 		
 		this.secureCommonHeaderSerialized = secureCommonHeaderSerialized;
+
 		this.versionID = secureCommonHeaderSerialized[0];
 		this.messageType = secureCommonHeaderSerialized[1];
+		this.timestamp = CommonUtils.fromByteArrayToLong
+						(new byte[] { secureCommonHeaderSerialized[2], secureCommonHeaderSerialized[3],
+						 			  secureCommonHeaderSerialized[4], secureCommonHeaderSerialized[5],
+									  secureCommonHeaderSerialized[6], secureCommonHeaderSerialized[7],
+									  secureCommonHeaderSerialized[8], secureCommonHeaderSerialized[9] });
 		
 	}
 	
@@ -38,6 +55,14 @@ public abstract class SecureCommonHeader {
 	
 	public void setMessageType(byte messageType) {
 		this.messageType = messageType;
+	}
+	
+	public long getTimestamp() {
+		return this.timestamp;
+	}
+	
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
 	}
 	
 	public byte[] getSecureCommonHeaderSerialized() {
