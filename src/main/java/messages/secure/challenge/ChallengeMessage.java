062cae1c1b5abb5d7045d234c1739a989f65e29b
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-29 22:24:28 +0000

    Challenge Message added

--- src/main/java/messages/secure/challenge/ChallengeMessage.java
+++ src/main/java/messages/secure/challenge/ChallengeMessage.java
@@ -1,7 +1,36 @@
 package main.java.messages.secure.challenge;
 
+import main.java.messages.secure.challenge.block.ChallengeMessageBlock;
+import main.java.messages.secure.challenge.metaheader.ChallengeMessageMetaHeader;
+
 public class ChallengeMessage {
 	
+	private ChallengeMessageMetaHeader challengeMessageMetaHeader;
 	
+	private ChallengeMessageBlock challengeMessageBlock;
+	
+	
+	public ChallengeMessage(ChallengeMessageMetaHeader challengeMessageMetaHeader, ChallengeMessageBlock challengeMessageBlock) {
+		
+		this.challengeMessageMetaHeader = challengeMessageMetaHeader;
+		this.challengeMessageBlock = challengeMessageBlock;
+		
+	}
+	
+	public ChallengeMessageMetaHeader getChallengeMessageMetaHeader() {
+		return this.challengeMessageMetaHeader;
+	}
+	
+	public void setChallengeMessageMetaHeader(ChallengeMessageMetaHeader challengeMessageMetaHeader) {
+		this.challengeMessageMetaHeader = challengeMessageMetaHeader;
+	}
+	
+	public ChallengeMessageBlock getChallengeMessageBlock() {
+		return this.challengeMessageBlock;
+	}
+	
+	public void setChallengeMessageBlock(ChallengeMessageBlock challengeMessageBlock) {
+		this.challengeMessageBlock = challengeMessageBlock;
+	}
 	
 }
