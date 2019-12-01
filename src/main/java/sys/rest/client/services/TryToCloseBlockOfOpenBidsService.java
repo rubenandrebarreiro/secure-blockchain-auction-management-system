5b29bf68b2e30f80a6deb3b945403931b083b495
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-01 17:52:20 +0000

    Some minor improvements added

--- src/main/java/sys/rest/client/services/TryToCloseBlockOfOpenBidsService.java
+++ src/main/java/sys/rest/client/services/TryToCloseBlockOfOpenBidsService.java
@@ -1,5 +1,11 @@
 package main.java.sys.rest.client.services;
 
-public class TryToCloseBlockOfOpenBidsService {
+public class TryToCloseBlockOfOpenBidsService implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
