72ae9c9473545d43afdcac2a86865568b7f1007f
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-12-03 19:01:27 +0000

    Update script to check args

--- runClient.sh
+++ runClient.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: $0 <hostURL> <hostSslSocketPort>"
+  exit 1
+fi
+
 mvn exec:java -Dexec.mainClass="main.java.sys.rest.client.Client" -Dexec.args="$1 $2"
