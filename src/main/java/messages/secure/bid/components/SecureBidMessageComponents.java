b8018dd8029aad825d4ea9fcbedbe2f5f4d740a9
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-04 12:06:36 +0000

    Secure Bid Message Components added

---
+++ src/main/java/messages/secure/bid/components/SecureBidMessageComponents.java
@@ -0,0 +1,13 @@
+package main.java.messages.secure.bid.components;
+
+import main.java.messages.secure.bid.data.SecureBidMessageData;
+import main.java.messages.secure.common.header.SecureCommonHeader;
+
+public class SecureBidMessageComponents {
+
+	private SecureCommonHeader secureCommonHeader;
+	
+	private SecureBidMessageData secureBidMessageData;
+	
+	
+}
