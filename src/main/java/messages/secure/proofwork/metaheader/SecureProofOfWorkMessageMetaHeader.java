c46e47cedf56a8ad31ec9498df7786ae1babff03
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-11 10:59:14 +0000

    Some minor changes in Secure Proof Of Work Message's MetaHeader added

--- src/main/java/messages/secure/proofwork/metaheader/SecureProofOfWorkMessageMetaHeader.java
+++ src/main/java/messages/secure/proofwork/metaheader/SecureProofOfWorkMessageMetaHeader.java
@@ -2,6 +2,33 @@ package main.java.messages.secure.proofwork.metaheader;
 
 public class SecureProofOfWorkMessageMetaHeader {
 	
+	private int sizeOfUserPeerIDSerialized;
 	
+	private int sizeOfSecureProofOfWorkMessageKeyExchangeSerializedCiphered;
+	private int sizeOfSecureProofOfWorkMessageKeyExchangeSerializedCipheredSigned;
+	
+	private int sizeOfSecureProofOfWorkMessageSolvedBlockSerialized;
+	private int sizeOfSecureProofOfWorkMessageDoSMitigationSerialized;
+	  
+	private int sizeOfSecureBidMessageDataSignatureSerialized;
+	private int sizeOfSecureBidMessageDataPersonalSerializedCipheredAndHashed;
+	  
+	private int sizeOfProofOfWorkSerialized;
+	private int sizeOfBidSerializedDigitalSigned;
+	  
+	private int sizeOfBidderUserClientIDSerialized;
+	  
+	private int sizeOfSecureBidMessageDataPersonalSerializedCiphered;
+	private int sizeOfSecureBidMessageDataPersonalSerializedCipheredHashed;
+	private int sizeOfSecureBidMessageDataPersonalSerialized;
+	  
+	private int sizeOfUserEmailSerialized;
+	private int sizeOfUserHomeAddressSerialized;
+	private int sizeOfUserBankAccountNIBSerialized;
+	
+	
+	private byte[] secureBidMessageMetaHeaderSerialized;
+	
+	private boolean isSecureBidMessageMetaHeaderSerialized;
 	
 }
