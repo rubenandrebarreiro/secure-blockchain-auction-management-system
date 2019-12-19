111720b0b5918f58c0c38886581ba6a701ed427f
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-12-19 17:11:01 +0000

    Force using specific port

--- PACKAGE/runAuctionServerRepository.sh
+++ PACKAGE/runAuctionServerRepository.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]
-then
-  echo "Usage: $0 <port> "
-  exit 1
-fi
+
+#if [ "$#" -ne 1 ]
+#then
+#  echo "Usage: $0 <port> "
+#  exit 1
+#fi
 
 cd ..
-mvn exec:java -Dexec.mainClass="main.java.sys.rest.server.auction.repository.AuctionRepositoryServer" -Dexec.args="$1"
+mvn exec:java -Dexec.mainClass="main.java.sys.rest.server.auction.repository.AuctionRepositoryServer" -Dexec.args="8080"
 cd PACKAGE
